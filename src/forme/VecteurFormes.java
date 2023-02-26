ae6861de256c7748494840f1f8fdfed157d8efe1
Yasmine Karassane <2138750@cegeplimoilou.ca>
2023-02-25 23:35:35 -0500

    Attribut vecteur et méthode remplir
    •	Un attribut vecteur (ArrayList) de formes qui pourra contenir le nombre de formes voulues;
    •	Lors de la construction du VecteurFormes le vecteur restera vide.
    •	Une méthode « remplir » permet de remplir le vecteur avec des formes générées de la façon suivante

--- src/forme/VecteurFormes.java
+++ src/forme/VecteurFormes.java
@@ -2,6 +2,38 @@ package forme;
 
 import forme.IVecteurFormes;
 
+import java.util.ArrayList;
+
 public abstract class VecteurFormes implements IVecteurFormes {
 
+    private ArrayList<Forme> vecteur;
+
+    public void remplir(int nbrElements) throws ArrayIndexOutOfBoundsException {
+        if (nbrElements > 0) {
+            for (int i = 0; i < nbrElements; i++) {
+
+                for (int j = 0; j < Couleur.values().length; j++) {
+                    Forme cercle = new Cercle(5);
+                    getVecteur().set(i, cercle);
+                    cercle.setCouleur(Couleur.values()[j]);
+                    i++;
+                    if (nbrElements < i) {
+                        Forme rectangle = new Rectangle(5, 5);
+                        getVecteur().set(i, rectangle);
+                        i++;
+                        rectangle.setCouleur(Couleur.values()[j]);
+                    }
+                    if (nbrElements < i) {
+                        Forme triangle = new Triangle(5, 5, 5);
+                        getVecteur().set(i, triangle);
+                        i++;
+                        triangle.setCouleur(Couleur.values()[j]);
+                    }
+                }
+
+            }
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
 }
