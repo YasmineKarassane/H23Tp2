ca966993dbd470810ae6b05831cd3cc7cb3bb987
Yasmine Karassane <2138750@cegeplimoilou.ca>
2023-03-01 10:25:28 -0500

    VecteurFormes javadoc

--- src/forme/VecteurFormes.java
+++ src/forme/VecteurFormes.java
@@ -5,10 +5,18 @@ import forme.IVecteurFormes;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Interface de manipulation d'un vecteur basé sur un ArrayList
+ */
 public class VecteurFormes implements IVecteurFormes {
 
     private ArrayList<Forme> vecteur;
 
+    /**
+     * Obtenir le vecteur de formes avec le nombre de forme
+     *
+     * @return le vecteur de formes
+     */
     @Override
     public ArrayList<Forme> getVecteur() {
         return null;
@@ -19,6 +27,21 @@ public class VecteurFormes implements IVecteurFormes {
         getVecteur().add(forme);
     }
 
+    /**
+     * Mettre le nombre de formes voulues dans le vecteur. Les formes qui seront
+     * placées dans le vecteur devront être générées de la façon suivante:
+     * <p>
+     * Tant que le nombre de formes souhaitées n'est pas atteint on crée une forme
+     * à la fois, pour toutes les formes disponibles (Cercle, Rectangle et
+     * Triangle) combiné à toutes les couleurs possibles (rouge, vert, bleu,
+     * jaune, noir, orange). Quand toutes les combinaisons sont épuisées on
+     * recommence.
+     * <p>
+     * Le but final étant d'obtenir le bon nombre de formes, les plus
+     * diversifiées possibles.
+     *
+     * @param nbrElements le nombre de formes voulues
+     */
     public void remplir(int nbrElements) throws ArrayIndexOutOfBoundsException {
         if (nbrElements > 0) {
 
@@ -39,6 +62,9 @@ public class VecteurFormes implements IVecteurFormes {
         }
     }
 
+    /**
+     * Mélanger les éléments du vecteur.
+     */
     public void melanger() {
         for (int i = 0; i < getVecteur().size(); i++) {
             Random chiffreRandom = new Random();
@@ -48,6 +74,10 @@ public class VecteurFormes implements IVecteurFormes {
         }
     }
 
+    /**
+     * Trier en ordre croissant le vecteur de formes en utilisant le "compareTo"
+     * disponible sur les objets.
+     */
     public void trier() {
         int enOrdre;
         do {
