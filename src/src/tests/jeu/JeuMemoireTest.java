88aae4a39d56acfc2a6800062ee427bdf4bc5c80
Gabriel Riverin <gabkanuna@gmail.com>
2023-03-06 19:35:26 -0500

    testGetVecteur et testGetNiveau tester et fonctionne

--- src/src/tests/jeu/JeuMemoireTest.java
+++ src/src/tests/jeu/JeuMemoireTest.java
@@ -9,24 +9,30 @@ class JeuMemoireTest {
 
     @Test
     void getGrille() {
+        JeuMemoire jeu = new JeuMemoire();
     }
 
     @Test
     void getNiveau() {
-
+        JeuMemoire jeu = new JeuMemoire();
+        assertEquals(1, jeu.getNiveau());
+        jeu.setNiveauPlusUn();
+        assertEquals(2, jeu.getNiveau());
     }
 
     @Test
     void getNomForme() {
         JeuMemoire jeu1 = new JeuMemoire();
-        System.out.println(jeu1.getNomForme(1,1));
-        System.out.println(jeu1.getNomForme(1,2));
-        System.out.println(jeu1.getNomForme(2,1));
-        System.out.println(jeu1.getNomForme(2,4));
+        jeu1.getVecteur().trier();
+
+        assertEquals("Cerclebleu", jeu1.getNomForme(1, 1));
     }
 
     @Test
     void getVecteur() {
+        JeuMemoire jeu = new JeuMemoire();
+        jeu.getVecteur().trier();
+        assertEquals(" Cercle bleu Cercle jaune Cercle noir Cercle orange Cercle rouge Cercle rouge Cercle vert Rectangle bleu Rectangle jaune Rectangle noir Rectangle orange Rectangle rouge Rectangle rouge Rectangle vert Triangle bleu Triangle jaune Triangle noir Triangle orange Triangle rouge Triangle vert", jeu.getVecteur().toString());
     }
 
     @Test
